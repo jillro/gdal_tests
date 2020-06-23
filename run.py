561d2c4b75bde827b6fce4d6c3b2875a9460049d
Jill Royer <hello@jillroyer.me>
2020-06-23 18:03:22 +0200

    we need to print it

--- run.py
+++ run.py
@@ -2,4 +2,5 @@ from django.contrib.gis.geos import GEOSGeometry
 
 assert GEOSGeometry('POINT(2.350643 48.880848)', srid=4326).ewkt == 'SRID=4326;POINT (2.350643 48.880848)'
 
+print(GEOSGeometry('POINT(2.350643 48.880848)', srid=4326).transform(3857, True).ewkt)
 assert GEOSGeometry('POINT(2.350643 48.880848)', srid=4326).transform(3857, True).ewkt == 'SRID=3857;POINT (261672.381796773 6254667.92282535)'
